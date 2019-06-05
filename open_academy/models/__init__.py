901d84586343984b3766c3465a911cc82b1acb06
Sean <Sean@SeanGahan.Net>
2019-06-05 10:00:54 -0700

    trying to fix

--- open_academy/models/__init__.py
+++ open_academy/models/__init__.py
@@ -2,4 +2,4 @@
 
 from . import course
 from . import session
-from . import person
+# from . import person
--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -2,22 +2,6 @@
 
 from odoo import models, fields, api
 
-# class open_academy(models.Model):
-#     _name = 'open_academy.open_academy'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
-
-
-
-#from odoo import models,fields,api
-
 class Course(models.Model):
     _name = 'open_academy.course'
     _description = 'Course Info'
@@ -28,4 +12,4 @@ class Course(models.Model):
         ('easy','Easy'),
         ('normal','Normal'),
         ('hard','Hard')
-    ])
+    ],string="Diffiulty")
