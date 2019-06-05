1fa7ce96f9adec78dc92f355af0ba85ed530e495
Sean-AOF <51309677+Sean-AOF@users.noreply.github.com>
2019-06-05 22:25:36 +0000

    stuff

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -9,5 +9,13 @@ class Session(models.Model):
     name = fields.Char('Session_Name', required=True)    
     date_start = fields.Date(string='Start Date')
     end_date = fields.Date(string = 'End Date')
-    course = fields.Many2one('open_academy.course', required=True)
+    course_id = fields.Many2one('open_academy.course', required=True)
     #teacher = fields.Many2one('open_academy.person', required=True)
+    
+#     course_name = fileds.Char(compute ='_compute_get_course_names')#
+    
+#     def _compute_course_names(self):
+#         courses = self.mapped('course_id')
+#         cources_uniquie= courses | course #union
+        
+        
