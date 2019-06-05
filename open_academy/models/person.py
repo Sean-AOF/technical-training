a3e43efe173e3cafde0a870ceea9484dffa211d7
Sean-AOF <51309677+Sean-AOF@users.noreply.github.com>
2019-06-05 18:24:56 +0000

    fixing stoff

--- open_academy/models/person.py
+++ open_academy/models/person.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+
+from odoo import models, fields, api, _
 
 class Person(models.Model):
     _name = 'open_academy.person'
     _description = 'Person Info'
-    _rec_name = 'title'
-
     
     first_name = fields.Char(string ='First Name',require=True)
     last_name = fields.Char(string='Last Name',require=True)
-    # name = fields.Char(compute='_compute_name')
+
+    person_typ=fields.Selection([
+        ('teacher','Teacher'),
+        ('student','Student'),
+    ],string='Type')
     
-    # @api.depends('first_name','last_name')
-    # def _compute_name(self):
-    #     for rec in self
-    #         rec.name="{} {}".format(rec.first_name,rec.last_name)
+    name = fields.Char(compute='_compute_name', store=True)
+    
+    
+#     @api.depends('first_name','last_name')
+#     def _compute_name(self):
+#         for rec in self
+#             rec.name="{} {}".format(rec.first_name,rec.last_name)
 
 
 		
